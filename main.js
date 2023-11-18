484e8dac557a2d385e8cd855683d1397c24fea84
nodirbek984 <nodirbeknegmatov2008@gmail.com>
2023-11-18 13:55:11 +0500

    tugatildi

---
+++ main.js
@@ -0,0 +1,127 @@
+//function1
+// function sum1(x, y) {
+//   let resoult = x + y;
+//   return resoult;
+// }
+
+// let a = sum1(3, 5);
+
+//function2
+// let min = +prompt("Nechi minut");
+// function toSekund(sekund) {
+//   let resoult2 = sekund * 60;
+//   return resoult2;
+// }
+
+// let natija2 = toSekund(min);
+
+//function3
+// let b = +prompt("sonni kiriting");
+// function incSum(s) {
+//   let resoult3 = s + 1;
+//   return resoult3;
+// }
+// let natija3 = incSum(b);
+// alert(natija3);
+
+//function4
+// let asosi = +prompt("Asosini kiriting");
+// let balandlik = +prompt("Balandligini kiriting");
+
+// function S(a, b) {
+//   let resoult = (a * b) / 2;
+//   return resoult;
+// }
+
+// let natija = S(asosi, balandlik);
+// alert(natija);
+
+//function5
+// let yosh = +prompt("yoshinginni kiriting");
+
+// function toYear(a) {
+//   return a * 365;
+// }
+
+// let resoult = toYear(yosh)
+// alert(resoult)
+
+//function6
+// let son = +prompt("sonni kiriting");
+
+// function sum(a) {
+//   return a *a * a;
+// }
+// let resoult = sum(son)
+// alert(resoult)
+
+//function7
+// function sum(a, b, c) {
+//   return a;
+// }
+// let resoult = sum(2, 3, 5)
+// alert(resoult)
+
+//function8
+// let son1 = +prompt("1-son");
+// let son2 = +prompt("2-son");
+
+// function sum(x, y) {
+//   return x * y;
+// }
+// let resoult = sum(son1, son2);
+// alert(resoult);
+
+//function9
+// let soat = +prompt("nechi soat");
+
+// function sum(x) {
+//   return x * 3600;
+// }
+// let resoult = sum(soat);
+// alert(resoult);
+
+//function10
+// let son1 = +prompt("1-son");
+// let son2 = +prompt("2-son");
+
+// function sum(x, y) {
+//   return x + y -1;
+// }
+// let resoult = sum(son1, son2);
+// alert(resoult);
+
+//function11
+// let son1 = +prompt("son1")
+// let son2 = +prompt('son2')
+// function sum (x, y){
+//     return y % x
+// }
+// let resoult = sum(son1, son2)
+// alert(resoult)
+
+//function12
+// let boy = +prompt("boyi");
+// let eni = +prompt("eni");
+// function sum(x, y) {
+//   return x * y;
+// }
+// let resoult = sum(boy, eni);
+// alert(resoult)
+
+//function13
+// let gap = prompt("Sozni yozing");
+// function sum(a) {
+//   let b = `something ${a}`;
+//   return b;
+// }
+// let resoult = sum(gap);
+// alert(resoult);
+
+//function14
+// let son = prompt("Sonni yozing");
+// function sum(a) {
+//   return a * a;
+// }
+// let resoult = sum(son);
+// alert(resoult);
